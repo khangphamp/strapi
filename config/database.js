077ac80f7af1e3af0baf7f9a0946fbb278d22925
galaway <84115190+galaway@users.noreply.github.com>
2021-11-11 15:02:50 +0700

    commit 3

--- config/database.js
+++ config/database.js
@@ -1,20 +1,15 @@
-
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
-        client: "postgres",
-        host: env("PGHOST", "localhost"),
-        port: env.int("PGPORT", 5432),
-        database: env("PGDATABASE", "strapi"),
-        username: env("PGUSER", "strapi"),
-        password: env("PGPASSWORD", "strapi"),
-        schema: env("DATABASE_SCHEMA", "public"), // Not Required
-        ssl: false,
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      options: {},
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
